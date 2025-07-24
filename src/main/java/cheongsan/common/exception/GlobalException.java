a1798470afecd75f0ef5991ae8ced87cb3751817
ockko <ocko119@gmail.com>
2025-07-24 18:50:15 +0900

    feat: 전역 예외 처리 클래스 구현 #23

--- src/main/java/cheongsan/common/exception/GlobalException.java
+++ src/main/java/cheongsan/common/exception/GlobalException.java
@@ -1,4 +1,31 @@
 package cheongsan.common.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalException {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponseDto> handleIllegalStateException(IllegalStateException e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto("서버 내부 오류가 발생했습니다.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // --- 에러 응답을 위한 DTO 클래스 ---
+    @AllArgsConstructor
+    @Getter
+    private static class ErrorResponseDto {
+        private final String message;
+    }
 }
