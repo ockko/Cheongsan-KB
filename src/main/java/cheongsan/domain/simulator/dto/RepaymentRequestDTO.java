93dfc9f9113175b3b25a6387ab3befe05c4d7a74
wotns <136947201+wotns@users.noreply.github.com>
2025-07-25 09:57:45 +0900

    refactor: DTO 이름 변경 #2

--- src/main/java/cheongsan/domain/simulator/dto/RepaymentRequestDTO.java
+++ src/main/java/cheongsan/domain/simulator/dto/RepaymentRequestDTO.java
@@ -1,17 +1,19 @@
 package cheongsan.domain.simulator.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RepaymentRequestDto {
-    private List<LoanDto> loans; // 사용자의 대출 리스트
+public class RepaymentRequestDTO {
+    private List<LoanDTO> loans; // 사용자의 대출 리스트
     private BigDecimal monthlyPayment; // 기존 월 상환액
     private BigDecimal extraPaymentAmount; // 추가 상환액 (nullable 또는 0)
     private BigDecimal monthlyAvailableAmount;
