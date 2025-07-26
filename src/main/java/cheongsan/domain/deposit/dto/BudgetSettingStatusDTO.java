37dd0a17ff38091265dbdc0864fd86e58cef4c47
ockko <ocko119@gmail.com>
2025-07-26 10:51:08 +0900

    Refactor: DTO에 @Getter를 @Data로 변경 #43

--- src/main/java/cheongsan/domain/deposit/dto/BudgetSettingStatusDTO.java
+++ src/main/java/cheongsan/domain/deposit/dto/BudgetSettingStatusDTO.java
@@ -2,12 +2,13 @@ package cheongsan.domain.deposit.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
-@Getter
+@Data
 public class BudgetSettingStatusDTO {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
