79b4efc653838ffb8956fafda8810281eb155d00
kirikaka <kiridodo0714@gmail.com>
2025-07-29 00:32:01 +0900

    feat: 회원가입API 구현 및 testcode 추가 #35

--- src/main/java/cheongsan/domain/user/dto/SignUpRequestDTO.java
+++ src/main/java/cheongsan/domain/user/dto/SignUpRequestDTO.java
@@ -4,10 +4,12 @@ package cheongsan.domain.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignUpRequestDTO {
     private String userId;
     private String password;
