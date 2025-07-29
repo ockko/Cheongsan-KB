2d8d2fc7a8f89292b1e43035c871fa0ca190ebb8
Hajuuu <hpt0127@naver.com>
2025-07-29 11:09:21 +0900

    feat: 필드 변경 #41
    연이율 필드 %에서 소수점으로 변경 및 repaymentType 필드 추가

--- src/main/java/cheongsan/domain/simulator/dto/LoanDTO.java
+++ src/main/java/cheongsan/domain/simulator/dto/LoanDTO.java
@@ -17,9 +17,10 @@ public class LoanDTO {
     private Long id;
     private String loanName; // 대출명
     private BigDecimal principal; // 남은 원금
-    private double interestRate; // 연이율(%)
+    private double interestRate; // 연이율(소수점)
     private BigDecimal monthlyPayment; // 월 납입금
     private LocalDate startDate; // 대출 시작일
     private LocalDate endDate; // 대출 만기일
     private String institutionType; // 금융1, 금융2, 기타
+    private RepaymentType repaymentType; // 예: "원리금균등", "원금균등", "만기일시"
 }
