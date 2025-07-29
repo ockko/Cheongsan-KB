19d26ef43302afa72113bf9da2143e39092e6654
kirikaka <kiridodo0714@gmail.com>
2025-07-29 10:12:25 +0900

    refactor: 비밀번호 암호화 및 connectedId 랜덤 생성 #35

--- src/main/java/cheongsan/domain/user/service/AuthServiceImpl.java
+++ src/main/java/cheongsan/domain/user/service/AuthServiceImpl.java
@@ -9,13 +9,18 @@ import cheongsan.domain.user.entity.User;
 import cheongsan.domain.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AuthServiceImpl implements AuthService {
     private final UserMapper userMapper;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     @Override
     public SignUpResponseDTO signUp(SignUpRequestDTO signUpRequestDTO) {
@@ -26,10 +31,10 @@ public class AuthServiceImpl implements AuthService {
         if (userMapper.findByEmail(signUpRequestDTO.getEmail()) != null) {
             throw new IllegalArgumentException("중복된 이메일입니다.");
         }
-
+        String encodedPw = passwordEncoder.encode(signUpRequestDTO.getPassword());
         User user = User.builder()
                 .userId(signUpRequestDTO.getUserId())
-                .password(signUpRequestDTO.getPassword())
+                .password(encodedPw)
                 .email(signUpRequestDTO.getEmail())
                 .connectedId(genereateConnectId(signUpRequestDTO.getUserId(), signUpRequestDTO.getPassword()))
                 .build();
@@ -40,9 +45,10 @@ public class AuthServiceImpl implements AuthService {
     }
 
     public String genereateConnectId(String userId, String password) {
-
-        return "connectedID";
+        // connectedId 랜덤 생성.
+        return UUID.randomUUID().toString();
     }
+
 
     @Override
     public FindUserIdResponseDTO findUserIdByEmail(FindUserIdRequestDTO findUserIdRequestDTO) {
