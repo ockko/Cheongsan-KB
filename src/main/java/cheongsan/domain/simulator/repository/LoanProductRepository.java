a919b138b684f45084c8c67a16a3f763242b5767
wotns <136947201+wotns@users.noreply.github.com>
2025-07-29 16:17:20 +0900

    refactor: 필요없는 파람 삭제 #46

--- src/main/java/cheongsan/domain/simulator/repository/LoanProductRepository.java
+++ src/main/java/cheongsan/domain/simulator/repository/LoanProductRepository.java
@@ -1,17 +1,13 @@
 package cheongsan.domain.simulator.repository;
 
 import cheongsan.domain.simulator.dto.LoanProductDTO;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Mapper
 public interface LoanProductRepository {
     List<LoanProductDTO> findRecommendedLoans(
-            @Param("principal") BigDecimal principal,
-            @Param("userDsr") BigDecimal userDsr,
-            @Param("dsrLimit") BigDecimal dsrLimit
+            @Param("principal") BigDecimal principal
     );
 }
