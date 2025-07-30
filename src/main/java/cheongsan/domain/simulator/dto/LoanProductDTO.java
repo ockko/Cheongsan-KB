97a9040f938ac88ec90a3b8a7c62ef7e7d8a13e3
wotns <136947201+wotns@users.noreply.github.com>
2025-07-30 13:48:51 +0900

    refactor: 상품 금리, 최대한도 타입 변경 #46

--- src/main/java/cheongsan/domain/simulator/dto/LoanProductDTO.java
+++ src/main/java/cheongsan/domain/simulator/dto/LoanProductDTO.java
@@ -2,16 +2,16 @@ package cheongsan.domain.simulator.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoanProductDTO {
     private String productName;       // 상품명
     private String institutionName;   // 기관명
-    private BigDecimal interestRate;  // 상품 금리
-    private BigDecimal loanLimit;     // 최대 한도
+    private String interestRate;      // 상품 금리
+    private String loanLimit;         // 최대 한도
     private String siteUrl;           // 사이트 URL
     private String dsrCheck;          // DSR 적용 여부('Y' or 'N')
 }
