85b2327da21dd8ece333c883f43499db9037f965
wotns <136947201+wotns@users.noreply.github.com>
2025-07-30 15:03:09 +0900

    refactor: @Getter로 수정 #46

--- src/main/java/cheongsan/domain/simulator/entity/Simulator.java
+++ src/main/java/cheongsan/domain/simulator/entity/Simulator.java
@@ -1,10 +1,10 @@
 package cheongsan.domain.simulator.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Simulator {
