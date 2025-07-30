a11efd747cf63624221f4eec7eb70bcd2d421d2d
JWMin556 <min981626@gmail.com>
2025-07-30 18:18:03 +0900

    feat: 사용자 ID와 계좌번호로 대출계좌 조회 기능 debt도메인에 추가  #34

---
+++ src/main/java/cheongsan/domain/debt/mapper/DebtTransactionMapper.java
@@ -0,0 +1,27 @@
+package cheongsan.domain.debt.mapper;
+
+import cheongsan.domain.debt.entity.DebtTransaction;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Mapper
+public interface DebtTransactionMapper {
+    //대출 거래내역 저장
+    void insertDebtTransaction(DebtTransaction debtTransaction);
+
+    // 대출계좌별 거래내역 조회
+    List<DebtTransaction> findByDebtAccountId(@Param("debtAccountId") Long debtAccountId);
+
+    // 대출 거래내역 중복 체크
+    boolean isDebtTransactionExists(@Param("debtAccountId") Long debtAccountId,
+                                    @Param("transactionDate") LocalDate transactionDate);
+
+    // 기간별 대출 거래내역 조회
+    List<DebtTransaction> findByDebtAccountIdAndDateRange(
+            @Param("debtAccountId") Long debtAccountId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
+}
