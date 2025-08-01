d4e392d9df3ea5ce84c957a78348b15078c9570e
Hajuuu <hpt0127@naver.com>
2025-08-01 17:17:30 +0900

    refactor: 필드 추가 #70
    중도 상환 관련 필드 추가, 납부일 관련 필드 추가

--- src/main/java/cheongsan/domain/simulator/dto/MonthlyPaymentDetailDTO.java
+++ src/main/java/cheongsan/domain/simulator/dto/MonthlyPaymentDetailDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
@@ -11,5 +12,9 @@ public class MonthlyPaymentDetailDTO {
     private long month; // 몇 번째 상환월인지 (1 ~ 상환 개월 수)
     private BigDecimal principal; // 해당 월에 상환한 원금
     private BigDecimal interest; //  해당 월에 발생한 이자
+    private BigDecimal prepayment; // 중도상환 금액
+    private BigDecimal prepaymentFee; // 중도상환 수수료
     private BigDecimal totalPayment; // 해당 월의 총 납입액 (원금 + 이자)
+    private LocalDate paymentDate; // 실제 납입 날
+    private BigDecimal remainingPrincipal; // 이달 납부 후 남은 원금
 }
