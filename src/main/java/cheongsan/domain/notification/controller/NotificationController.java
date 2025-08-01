8fc220da2bda8d919a3c60eee9d4ca8e8a63f74e
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-08-01 16:50:29 +0900

    fix: Cr@ossOrigin 삭제 #61

--- src/main/java/cheongsan/domain/notification/controller/NotificationController.java
+++ src/main/java/cheongsan/domain/notification/controller/NotificationController.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:8080") // 지워야 함.
 @RestController
 @RequestMapping("/cheongsan/notifications")
 @RequiredArgsConstructor
