c162a511835af93e6e33361874fc5c02b8049132
Hajuuu <hpt0127@naver.com>
2025-08-01 17:13:34 +0900

    refactor: 필드 추가 #70
    request에 사용하지 않는 필드 삭제, response에 상환 내역 리스트와 중도 상환 관련 필드, 만기 일시 필드 추가

--- src/main/java/cheongsan/domain/simulator/dto/RepaymentRequestDTO.java
+++ src/main/java/cheongsan/domain/simulator/dto/RepaymentRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -13,8 +14,6 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class RepaymentRequestDTO {
-    private List<LoanDTO> loans; // 사용자의 대출 리스트
-    private BigDecimal monthlyPayment; // 기존 월 상환액
-    private BigDecimal extraPaymentAmount; // 추가 상환액 (nullable 또는 0)
+    private List<LoanDTO> loans = new ArrayList<>();
     private BigDecimal monthlyAvailableAmount;
 }
---
+++ src/main/java/cheongsan/domain/simulator/dto/RepaymentResponseDTO.java
@@ -0,0 +1,27 @@
+package cheongsan.domain.simulator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RepaymentResponseDTO {
+    private StrategyType strategyType; // 어떤 전략
+    private long totalMonths; // 총 상환 기간(개월)
+    private BigDecimal interestSaved; // 절약 이자
+    private BigDecimal totalPayment; // 전략 적용 후 총 상환액
+    private BigDecimal originalPayment; // 원래 총 상환액
+    private BigDecimal totalPrepaymentFee; // 중도상환수수료 총합
+    private List<String> sortedLoanNames;
+    private Map<String, List<MonthlyPaymentDetailDTO>> repaymentHistory;
+    private Map<String, LocalDate> debtFreeDates; // 만기일시
+}
