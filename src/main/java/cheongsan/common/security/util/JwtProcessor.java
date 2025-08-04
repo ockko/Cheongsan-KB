b420bc228d3dc368798b8a4e6803d8adc4f2ae10
ockko <ocko119@gmail.com>
2025-08-04 16:16:17 +0900

    refactor: 토큰 유효 시간 및 비밀키 수정 #80
    - 개발 시 사용하던 토큰 유효 시간 및 비밀키를 실제 운영에 사용할 토큰 유효 시간과 비밀키로 수정

--- src/main/java/cheongsan/common/security/util/JwtProcessor.java
+++ src/main/java/cheongsan/common/security/util/JwtProcessor.java
@@ -3,25 +3,19 @@ package cheongsan.common.security.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtProcessor {
-    static private final long ACCESS_TOKEN_VALID_MILISECOND = 1000L * 60 * 2; // 2분
-    static private final long REFRESH_TOKEN_VALID_MILISECOND = 1000L * 60 * 2 * 2; // 4분
+    static private final long ACCESS_TOKEN_VALID_MILISECOND = 1000L * 60 * 60; // 1시간
+    static private final long REFRESH_TOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 24 * 14; // 2주
 
-//    static private final long ACCESS_TOKEN_VALID_MILISECOND = 1000L * 60 * 60; // 1시간 -- 운영시 사용
-//    static private final long REFRESH_TOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 24 * 14; // 2주 -- 운영시 사용
-
-    private final String secretKey = "충분히 긴 임의의(랜덤한) 비밀키 문자열 배정";
-    private final Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-
-//    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // -- 운영시 사용
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateAccessToken(String subject) {
         return createToken(subject, ACCESS_TOKEN_VALID_MILISECOND);
