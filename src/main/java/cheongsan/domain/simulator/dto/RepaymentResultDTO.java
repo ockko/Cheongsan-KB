3659083a98182338866cf7a6a0ed1da01f9b557a
Hajuuu <hpt0127@naver.com>
2025-08-05 16:26:49 +0900

    feat: 상환 시뮬레이션 결과 추가 #96
    - RepaymentResultDTO 추가
    - 기존 반환 값에 기존 상환액 총합과 월 중도 상환액, 총 상환액 추가하여 반환하도록 구현

---
+++ src/main/java/cheongsan/domain/simulator/dto/RepaymentResultDTO.java
@@ -0,0 +1,17 @@
+package cheongsan.domain.simulator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class RepaymentResultDTO {
+    private BigDecimal existingRepaymentAmount;
+    private BigDecimal additionalRepaymentAmount;
+    private BigDecimal totalRepaymentAmount;
+    private List<RepaymentResponseDTO> repayments = new ArrayList<>();
+}
