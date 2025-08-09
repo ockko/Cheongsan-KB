84a821d8ccaf4df442350b64837aed33acc07352
JWMin556 <min981626@gmail.com>
2025-08-09 16:56:16 +0900

    refactor: 네이버 로그인 관련 쿼리 Mapper에 추가 #113

--- src/main/java/cheongsan/domain/user/mapper/UserMapper.java
+++ src/main/java/cheongsan/domain/user/mapper/UserMapper.java
@@ -58,4 +58,7 @@ public interface UserMapper {
 
     void updateStrategy(@Param("id") Long id,
                         @Param("strategyName") String strategyName);
+
+    // 네이버 로그인 관련 메서드
+    User findByNaverId(@Param("naverId") String naverId);
 }
