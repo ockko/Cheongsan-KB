71ae42024a4afeb8bd15d820a083f22d8bf6ef08
wotns <136947201+wotns@users.noreply.github.com>
2025-08-09 19:33:29 +0900

    refactor: ResultDTO dsr 추가 #107

---
+++ src/main/java/cheongsan/domain/simulator/dto/RecommendationResultDTO.java
@@ -0,0 +1,14 @@
+package cheongsan.domain.simulator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class RecommendationResultDTO {
+    private final BigDecimal dsr;
+    private final List<LoanProductDTO> items;
+}
