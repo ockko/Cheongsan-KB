d3f860f8a16946cc11a6fa11ecf065bfd410928c
JWMin556 <min981626@gmail.com>
2025-08-11 17:59:44 +0900

    refactor: css 일부 수정 #25

--- src/components/domain/Signup/PasswordInput.vue
+++ src/components/domain/Signup/PasswordInput.vue
@@ -14,65 +14,29 @@ const emit = defineEmits([
   'validation-change',
 ]);
 
-// 비밀번호 유효성 검사 정규식 (8~16자, 영문 대소문자, 숫자, 특수문자 포함)
-const passwordRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+const updatePassword = (event) => {
+  const newPassword = event.target.value;
+  emit('update:password', newPassword);
 
-const passwordValidation = ref({
-  isValid: false,
-  showError: false,
-});
-
-const passwordConfirmValidation = ref({
-  isValid: false,
-  showError: false,
-});
-
-// 비밀번호 유효성 검사
-const validatePassword = () => {
-  passwordValidation.value.isValid = passwordRegex.test(props.password);
-  passwordValidation.value.showError =
-    props.password && !passwordValidation.value.isValid;
-
-  // 비밀번호가 변경되면 확인 필드도 다시 검사
-  if (props.passwordConfirm) {
-    validatePasswordConfirm();
-  }
-
-  emitValidation();
-};
-
-// 비밀번호 확인 유효성 검사
-const validatePasswordConfirm = () => {
-  passwordConfirmValidation.value.isValid =
-    props.passwordConfirm && props.password === props.passwordConfirm;
-  passwordConfirmValidation.value.showError =
-    props.passwordConfirm && !passwordConfirmValidation.value.isValid;
-
-  emitValidation();
-};
-
-// 유효성 검사 결과를 부모에게 전달
-const emitValidation = () => {
+  // 새로운 값을 기준으로 유효성 검사
   emit('validation-change', {
-    passwordValid: passwordValidation.value.isValid,
-    passwordConfirmValid: passwordConfirmValidation.value.isValid,
+    passwordValid: !!newPassword,
+    passwordConfirmValid:
+      !!props.passwordConfirm && newPassword === props.passwordConfirm,
   });
 };
 
-const updatePassword = (event) => {
-  emit('update:password', event.target.value);
-  validatePassword();
-};
-
 const updatePasswordConfirm = (event) => {
-  emit('update:passwordConfirm', event.target.value);
-  validatePasswordConfirm();
-};
+  const newPasswordConfirm = event.target.value;
+  emit('update:passwordConfirm', newPasswordConfirm);
 
-// props 변경 감지하여 유효성 검사 실행
-watch(() => props.password, validatePassword);
-watch(() => props.passwordConfirm, validatePasswordConfirm);
+  // 새로운 값을 기준으로 유효성 검사
+  emit('validation-change', {
+    passwordValid: !!props.password,
+    passwordConfirmValid:
+      !!newPasswordConfirm && props.password === newPasswordConfirm,
+  });
+};
 </script>
 
 <template>
@@ -88,11 +52,6 @@ watch(() => props.passwordConfirm, validatePasswordConfirm);
         :class="styles.input"
         :disabled="disabled"
       />
-      <!-- 비밀번호 유효성 검사 오류 메시지 -->
-      <div v-if="passwordValidation.showError" :class="styles.errorMessage">
-        <img src="/images/red-info.png" alt="비밀번호 유효성 검사 경고" />
-        8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해주세요
-      </div>
     </div>
 
     <!-- 비밀번호 확인 입력 -->
@@ -106,14 +65,6 @@ watch(() => props.passwordConfirm, validatePasswordConfirm);
         :class="styles.input"
         :disabled="disabled"
       />
-      <!-- 비밀번호 확인 오류 메시지 -->
-      <div
-        v-if="passwordConfirmValidation.showError"
-        :class="styles.errorMessage"
-      >
-        <img src="/images/red-info.png" alt="비밀번호 유효성 검사 경고" />
-        비밀번호가 일치하지 않습니다.
-      </div>
     </div>
   </div>
 </template>
