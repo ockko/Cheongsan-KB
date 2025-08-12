42bc25cc893cdcca2826c83ad0eb8b9228414ed1
kirikaka <kiridodo0714@gmail.com>
2025-08-12 10:33:15 +0900

    fix: fontawesome 추가 #33

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import "./assets/main.css";
+import './assets/main.css';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faFaceSmile, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faFaceSmile, faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faFaceSmile, faThumbsUp, faBell);
 
@@ -18,6 +18,6 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
-app.component("FontAwesomeIcon", FontAwesomeIcon);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 
-app.mount("#app");
+app.mount('#app');
