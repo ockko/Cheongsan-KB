f28b11d9b9bb4222ba3c5e81cefa72fd9ff52b5a
kirikaka <kiridodo0714@gmail.com>
2025-08-12 10:40:06 +0900

    fix: 메인 및 라이브러리에서 fontawesome관련 코드 삭제.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
+import './assets/main.css';
 
-import "./assets/main.css";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-
-import App from "./App.vue";
-import router from "./router";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faFaceSmile, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faFaceSmile, faThumbsUp, faBell);
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.component("FontAwesomeIcon", FontAwesomeIcon);
-
-app.mount("#app");
+app.mount('#app');
