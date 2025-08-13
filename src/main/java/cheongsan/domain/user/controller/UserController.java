7c2829b760fb64f6a95e4e32ec8703df68501f59
Hajuuu <hpt0127@naver.com>
2025-08-13 16:36:34 +0900

    refactor: merge 충돌 해결 후 기존 UserController 코드 추가 및 UserMapper 오타 삭제 #134

--- src/main/java/cheongsan/domain/user/controller/UserController.java
+++ src/main/java/cheongsan/domain/user/controller/UserController.java
@@ -2,10 +2,8 @@ package cheongsan.domain.user.controller;
 
 import cheongsan.common.constant.ResponseMessage;
 import cheongsan.common.exception.ResponseDTO;
-import cheongsan.domain.debt.service.DebtService;
 import cheongsan.domain.user.dto.*;
 import cheongsan.domain.user.entity.CustomUser;
-import cheongsan.domain.user.service.AuthService;
 import cheongsan.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,8 +18,6 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 public class UserController {
     private final UserService userService;
-    private final DebtService debtService;
-    private final AuthService authService;
 
     @GetMapping("/profile")
     public ResponseEntity<?> getMyProfile(@AuthenticationPrincipal CustomUser customUser) {
@@ -38,23 +34,24 @@ public class UserController {
         }
     }
 
-    @PatchMapping("/profile")
-    public ResponseEntity<?> updateMyProfile(
-            @RequestBody UpdateMyProfileRequestDTO request,
-            @AuthenticationPrincipal CustomUser customUser) {
-        try {
-            Long id = customUser.getUser().getId();
-
-            UpdateMyProfileResponseDTO response = userService.updateMyProfile(id, request);
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDTO(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO("서버 내부 오류가 발생했습니다."));
-        }
+    @PatchMapping("/profile/name")
+    public ResponseEntity<Void> updateName(@RequestBody UpdateNicknameRequestDTO request,
+                                           @AuthenticationPrincipal CustomUser customUser) {
+        Long id = customUser.getUser().getId();
+        userService.updateName(id, request);
+        return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/changePassword")
+    @PatchMapping("/profile/email")
+    public ResponseEntity<Void> updateEmail(@RequestBody UpdateEmailRequestDTO request,
+                                            @AuthenticationPrincipal CustomUser customUser) {
+        Long id = customUser.getUser().getId();
+        userService.updateEmail(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/profile/changePassword")
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequestDTO request,
                                             @AuthenticationPrincipal CustomUser customUser) {
         try {
@@ -70,6 +67,23 @@ public class UserController {
         }
     }
 
+    @PostMapping("/profile/verifyPassword")
+    public ResponseEntity<?> verifyPassword(
+            @RequestBody ChangePasswordRequestDTO request,
+            @AuthenticationPrincipal CustomUser customUser) {
+        try {
+            Long id = customUser.getUser().getId();
+            boolean isValid = userService.verifyPassword(id, request.getOldPassword());
+            return ResponseEntity.ok(isValid);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ResponseDTO(e.getMessage()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO("서버 내부 오류가 발생했습니다."));
+        }
+    }
+
 
     @DeleteMapping("/profile")
     public ResponseEntity<?> deleteMyAccount(
@@ -78,6 +92,7 @@ public class UserController {
     ) {
         try {
             Long id = customUser.getUser().getId();
+            System.out.println("회원 탈퇴 요청 - userId: " + id + ", password: " + request.getPassword());
             userService.deleteAccount(id, request);
             return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
         } catch (IllegalArgumentException e) {
@@ -88,23 +103,6 @@ public class UserController {
 
     }
 
-    @PostMapping("/nickname")
-    public ResponseEntity<?> submitNickname(@RequestBody NicknameRequestDTO request,
-                                            @AuthenticationPrincipal CustomUser customUser) {
-        try {
-
-            Long id = customUser.getUser().getId();
-            request.setUserId(id);
-
-            NicknameResponseDTO response = userService.submitNickname(request);
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDTO(e.getMessage()));
-        } catch (Exception e) {
-            log.info(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO("서버 내부 오류가 발생했습니다."));
-        }
-    }
 
     @PostMapping("/logout")
     public ResponseEntity<ResponseDTO> logout(@AuthenticationPrincipal CustomUser customUser) {
